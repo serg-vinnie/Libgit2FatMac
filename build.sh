3890fcda684dbbfca6dff82d5ee93a00253aabec
Sergiy Vynnychenko <sergiy@vynnychenko.com>
2021-04-28 03:08:38 +0300

    fixes

--- build.sh
+++ build.sh
@@ -21,18 +21,47 @@ make_openssl() {
 }
 
 make_libssh() {
-	export OPENSSL_ROOT_DIR="/tmp/openssl-"$1
-	env | grep SSL
-
 	local ARCH=$1
-	local root='/tmp/libssh-'$1
+	
+
+	local ARCH=$ARCH
+	local root='/tmp/libssh-'$ARCH
 	#rm -rf $root
 	local bin="${root}/lib/libssh2.a"
 	if [[ -f $bin ]]; then
-		echo "skipping libssh2 $1"
+		echo "skipping libssh2 $ARCH"
 	else		
-		echo "going to build libssh2 $1"
+		echo "going to build libssh2 $ARCH"
+		
+		export OPENSSL_ROOT_DIR="/tmp/openssl-"$ARCH
+		env | grep SSL
+		
 		cd libssh2
+		rm -rf ./build
+		mkdir build
+		cd build
+		cmake .. -DCMAKE_INSTALL_PREFIX=$root -DCMAKE_OSX_ARCHITECTURES=$ARCH
+		cmake --build . --target install -j $CPU_CORES_COUNT
+		cd ..
+	fi
+}
+
+make_libgit() {
+	local ARCH=$1
+	
+	local OPENSSL_ROOT_DIR="/tmp/openssl-"$ARCH
+	local libssh_root='/tmp/libssh-'$ARCH
+	export CMAKE_INCLUDE_PATH="${libssh_root}/include;${OPENSSL_ROOT_DIR}/include"
+	export CMAKE_LIBRARY_PATH="${libssh_root}/lib;${OPENSSL_ROOT_DIR}/lib"
+	
+	local root='/tmp/libgit-'$ARCH
+	#rm -rf $root
+	local bin="${root}/lib/libgit2.a"
+	if [[ -f $bin ]]; then
+		echo "skipping libgit2 $ARCH"
+	else		
+		echo "going to build libgit2 $ARCH"
+		cd libgit2
 		rm -rf ./build
 		mkdir build
 		cd build
@@ -47,3 +76,6 @@ make_openssl "x86_64"
 
 make_libssh "arm64"
 make_libssh "x86_64"
+
+make_libgit "arm64"
+make_libgit "x86_64"
